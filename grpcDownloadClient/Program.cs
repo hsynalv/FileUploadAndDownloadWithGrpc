16448dfcb931976f0fe666b9617def68181c6982
hhsynalv <alavhasan72892@gmail.com>
2022-09-08 19:02:29 +0300

    Add project files.

---
+++ grpcDownloadClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Net.Client;
+using grpcFileTransportDownloadClient;
+
+var channel = GrpcChannel.ForAddress("https://localhost:5001");
+var client = new FileService.FileServiceClient(channel);
+
+string downloadPath = @"C:\Users\beyazskorsky\Desktop\download\";
+
+var fileInfo = new grpcFileTransportDownloadClient.FileInfo()
+{
+    FileName = "gRPC",
+    FileExtension = ".pdf"
+};
+
+var request = client.FileDownload(fileInfo);
+FileStream fileStream = null;
+
+int count = 0;
+decimal chunkSize = 0;
+
+while (await request.ResponseStream.MoveNext(new CancellationTokenSource().Token))
+{
+    if (count++ == 0)
+    {
+        fileStream = new FileStream($"{downloadPath}/{request.ResponseStream.Current.Info.FileName}{request.ResponseStream.Current.Info.FileExtension}",FileMode.CreateNew);
+        
+        fileStream.SetLength(request.ResponseStream.Current.FileSize);
+    }
+
+    var buffer = request.ResponseStream.Current.Buffer.ToByteArray();
+    await fileStream.WriteAsync(buffer, 0, request.ResponseStream.Current.ReadedByte);
+
+    Console.WriteLine($"{Math.Round(((chunkSize += request.ResponseStream.Current.ReadedByte) * 100) / request.ResponseStream.Current.FileSize)}%");
+}
+
+Console.WriteLine("Download Başarılı");
+await fileStream.DisposeAsync();
+fileStream.Close();
---
+++ grpcServer/Services/FileTransportService.cs
@@ -0,0 +1,82 @@
+﻿using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using grpcFileTransportServer;
+using FileInfo = grpcFileTransportServer.FileInfo;
+
+namespace grpcServer.Services
+{
+    public class FileTransportService : FileService.FileServiceBase
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public FileTransportService(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public override async Task<Empty> FileUpload(IAsyncStreamReader<BytesContent> requestStream, ServerCallContext context)
+        {
+            string path = Path.Combine(_webHostEnvironment.WebRootPath, "Files");
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
+            FileStream fileStream = null;
+            try
+            {
+                int count = 0;
+                decimal chunkSize = 0;
+                
+                while (await requestStream.MoveNext())
+                {
+                    if (count++ == 0)
+                    {
+                        fileStream =
+                            new(
+                                $"{path}/{requestStream.Current.Info.FileName}{requestStream.Current.Info.FileExtension}",
+                                FileMode.CreateNew);
+                        fileStream.SetLength(requestStream.Current.FileSize);
+                    }
+
+                    var buffer = requestStream.Current.Buffer.ToByteArray();
+                    await fileStream.WriteAsync(buffer, 0, buffer.Length);
+                    Console.WriteLine($"{Math.Round(((chunkSize += requestStream.Current.ReadedByte) * 100) / requestStream.Current.FileSize)}%");
+                }
+            }
+            catch 
+            {
+            }
+
+
+            await fileStream.DisposeAsync();
+            fileStream.Close();
+            return new Empty();
+        }
+
+
+        public override async Task FileDownload(FileInfo request, IServerStreamWriter<BytesContent> responseStream, ServerCallContext context)
+        {
+            string path = Path.Combine(_webHostEnvironment.WebRootPath, "Files");
+
+            using FileStream fileStream = new($"{path}/{request.FileName}{request.FileExtension}",FileMode.Open,FileAccess.Read);
+
+            byte[] buffer = new byte[2048];
+
+            BytesContent content = new()
+            {
+                FileSize = fileStream.Length,
+                Info = new()
+                {
+                    FileName = Path.GetFileNameWithoutExtension(fileStream.Name),
+                    FileExtension = Path.GetExtension(fileStream.Name)
+                },
+                ReadedByte = 0
+            };
+
+            while ((content.ReadedByte = await fileStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+            {
+                content.Buffer = ByteString.CopyFrom(buffer);
+                await responseStream.WriteAsync(content);
+            }
+            fileStream.Close();
+        }
+    }
+}
